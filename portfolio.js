3d786a5cce88d8dd1f2d8d16f03299d5df66e2c5
거녕 <130534036+pachir1su@users.noreply.github.com>
2026-02-10 16:58:59 +0900

    Create portfolio.js

---
+++ portfolio.js
@@ -0,0 +1,75 @@
+AOS.init({
+  once: true,
+});
+
+VanillaTilt.init(document.querySelectorAll(".skill-card, .project-card"), {
+  max: 10,
+  speed: 400,
+  glare: true,
+  "max-glare": 0.5,
+});
+
+const mybutton = document.getElementById("btn-back-to-top");
+
+window.onscroll = function () {
+  scrollFunction();
+  updateProgressBar();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+function updateProgressBar() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+const typingText = document.getElementById("typing-text");
+const words = ["풀스택 개발자", "메이커", "아이디어 뱅크", "팀 리더"];
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+let typeSpeed = 100;
+
+function type() {
+  const currentWord = words[wordIndex];
+
+  if (isDeleting) {
+    typingText.textContent = currentWord.substring(0, charIndex - 1);
+    charIndex--;
+    typeSpeed = 50;
+  } else {
+    typingText.textContent = currentWord.substring(0, charIndex + 1);
+    charIndex++;
+    typeSpeed = 100;
+  }
+
+  if (!isDeleting && charIndex === currentWord.length) {
+    isDeleting = true;
+    typeSpeed = 2000;
+  } else if (isDeleting && charIndex === 0) {
+    isDeleting = false;
+    wordIndex = (wordIndex + 1) % words.length;
+    typeSpeed = 500;
+  }
+
+  setTimeout(type, typeSpeed);
+}
+
+document.addEventListener("DOMContentLoaded", type);
